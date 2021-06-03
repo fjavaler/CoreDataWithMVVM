4cd085d7d40dc8b10f4770a5327894a075fb6a90
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 13:52:19 -0600

    - Finished.

--- CoreDataWithMVVMDemo/ContentView.swift
+++ CoreDataWithMVVMDemo/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+  
+  // MARK: Properties
+  @StateObject var vm = ContentViewModel()
+  @State var textFieldText: String = ""
+  
+  // MARK: Body
+  var body: some View {
+    
+    NavigationView {
+      
+      VStack(spacing: 20) {
+        
+        TextField("Add fruit here...", text: $textFieldText)
+          .font(.headline)
+          .padding(.leading)
+          .frame(height: 55)
+          .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
+          .cornerRadius(10)
+          .padding(.horizontal)
+        
+        Button(action: {
+          guard !textFieldText.isEmpty else { return }
+          vm.addFruit(text: textFieldText)
+          textFieldText = ""
+        }, label: {
+          Text("Save".uppercased())
+            .font(.headline)
+            .foregroundColor(.white)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(Color(#colorLiteral(red: 1, green: 0.1857388616, blue: 0.5733950138, alpha: 1)))
+            .cornerRadius(10)
+        })
+        .padding(.horizontal)
+        
+        List {
+          
+          ForEach(vm.savedEntities) { entity in
+            Text(entity.name ?? "NO NAME")
+              .onTapGesture {
+                vm.updateFruit(entity: entity)
+              }
+          }//: End ForEach
+          .onDelete(perform: vm.deleteFruit)
+          
+        }//: List
+        .listStyle(PlainListStyle())
+      
+      }//: VStack
+      .navigationTitle("Fruits")
+    
+    }//: NavigationView
+  
+  }//: Body
 
+}//: View
+
+// MARK: Preview
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ CoreDataWithMVVMDemo/ViewModels/ContentViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  ContentViewModel.swift
+//  CoreDataWithMVVMDemo
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import Foundation
+import CoreData
+
+class ContentViewModel: ObservableObject {
+  
+  // MARK: Properties
+  let container: NSPersistentContainer
+  @Published var savedEntities: [FruitEntity] = []
+  
+  // MARK: Init
+  init() {
+    container = NSPersistentContainer(name: "FruitsContainer")
+    container.loadPersistentStores { description, error in
+      if let error = error {
+        print("ERROR LOADING CORE DATA. \(error)")
+      }
+    }
+    fetchFruits()
+  }
+  
+  // MARK: Functions
+  func fetchFruits() {
+    let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+    
+    do {
+      savedEntities = try container.viewContext.fetch(request)
+    } catch let error {
+      print("Error fetching. \(error)")
+    }
+  }
+  
+  // MARK: CRUD Operations
+  func addFruit(text: String) {
+    let newFruit = FruitEntity(context: container.viewContext)
+    newFruit.name = text
+    saveData()
+  }
+  
+  func updateFruit(entity: FruitEntity) {
+    let currentName = entity.name ?? ""
+    let newName = currentName + "!"
+    entity.name = newName
+    saveData()
+  }
+  
+  func deleteFruit(indexSet: IndexSet) {
+    // indexSet may contain many indices, but we know it's only one, therefore, we call .first. Stores index.
+    guard let index = indexSet.first else {return}
+    // Gets entity at index.
+    let entity = savedEntities[index]
+    // Delete the entity swipped.
+    container.viewContext.delete(entity)
+    // Save the remaining entities in the list after the delete.
+    saveData()
+  }
+  
+  func saveData() {
+    do {
+      try container.viewContext.save()
+      fetchFruits()
+    } catch let error {
+      print("Error saving. \(error)")
+    }
+  }
+}
