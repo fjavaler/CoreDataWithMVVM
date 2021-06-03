36b47678e2afe1d6d7e8ee8047370936b3d7f8df
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 13:02:57 -0600

    Initial Commit

---
+++ CoreDataWithMVVMDemo/CoreDataWithMVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataWithMVVMDemoApp.swift
+//  CoreDataWithMVVMDemo
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataWithMVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
